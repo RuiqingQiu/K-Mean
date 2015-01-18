3ef0933e9c25c68148cd900adb41e46b216b672d
Mingshan Wang <margartewm3@gmail.com>
2015-01-17 21:13:41 -0800

    Random function to create the initial board
     specify N to random generate the dataset, needs to read in the dataset
    later.

--- sample.py
+++ sample.py
@@ -4,38 +4,47 @@
 
 import numpy
 import matplotlib
+import random
+import numpy as np
 matplotlib.use('Agg')
 from scipy.cluster.vq import *
 import pylab
 pylab.close()
 
-path_to_image = '/Users/ruiqingqiu/Desktop/kmeans.png'
+def init_board(N):
+    X = np.array([(random.uniform(-1, 1), random.uniform(-1, 1)) for i in range(N)])
+    return X
+
+path_to_image = '/Users/margaretwm3/Desktop/kmeans.png'
 # generate 3 sets of normally distributed points around
 # different means with different variances
 # pt1 = numpy.random.normal(1, 0.2, (100,2))
 # pt2 = numpy.random.normal(2, 0.5, (300,2))
 # pt3 = numpy.random.normal(3, 0.2, (200,2))
-pt1 = numpy.random.normal(1, 0.2, (20,2))
-pt2 = numpy.random.normal(2, 0.5, (10,2))
-pt3 = numpy.random.normal(3, 0.2, (10,2))
+# pt1 = numpy.random.normal(1, 0.2, (20,2))
+# pt2 = numpy.random.normal(2, 0.5, (10,2))
+# pt3 = numpy.random.normal(3, 0.2, (10,2))
 
 # slightly move sets 2 and 3 (for a prettier output)
 #pt2[:,0] += 1
 #pt3[:,0] -= 0.5
 
-xy = numpy.concatenate((pt1, pt2, pt3))
-
-# kmeans for 3 clusters
-res, idx = kmeans2(numpy.array(zip(xy[:,0],xy[:,1])),5)
+#xy = numpy.concatenate((pt1, pt2, pt3))
+	
+#res, idx = kmeans2(numpy.array(zip(xy[:,0],xy[:,1])),5)
+dataSet = init_board(300); # create the initial configuration of the board
+print dataSet
+# kmeans for 5 clusters
+res, idx = kmeans2(dataSet,3)
 
 colors = ([([1,0,0],[0,1,0],[0,0,1],[1,1,0],[0,1,1])[i] for i in idx])
 
 # plot colored points
-pylab.scatter(xy[:,0],xy[:,1], c=colors)
+pylab.scatter(dataSet[:,0],dataSet[:,1], c=colors)
 
 # mark centroids as (X)
 pylab.scatter(res[:,0],res[:,1], marker='o', s = 500, linewidths=2, c='none')
 pylab.scatter(res[:,0],res[:,1], marker='x', s = 500, linewidths=2)
-print "here"
+#print "here"
 pylab.savefig(path_to_image)
 
