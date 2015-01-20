e373aa51cb4b757e06c7d3ea875e728521719bb5
Mingshan Wang <margartewm3@gmail.com>
2015-01-20 13:23:18 -0800

    Running hand written test progress

--- loader.py
+++ loader.py
@@ -54,7 +54,7 @@ class MNIST(object):
 
             image_data = array("B", file.read())
         # image data, same as 151
-        print image_data
+        #print image_data
 
         images = []
         for i in xrange(size):
