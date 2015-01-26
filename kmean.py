37962bd8625d968b474e47b1455a1b2af4321d84
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-01-26 12:35:36 -0800

    plot

--- kmean.py
+++ kmean.py
@@ -1,12 +1,13 @@
 #############################################################################
 # Full Imports
- 
+
 import sys
 import math
 import random
 import subprocess
 from loader import MNIST
- 
+from collections import Counter
+
 """
 This is a pure Python implementation of the K-Means Clustering algorithmn. The
 original can be found here:
@@ -30,31 +31,31 @@ To use plotly integration you will need to:
 2. Install the plotly module: pip install plotly
 
 """
- 
+
 PLOTLY_USERNAME = None
 PLOTLY_KEY = None
- 
+
 if PLOTLY_USERNAME:
     from plotly import plotly
- 
+
 def main():
-    
+
     # How many points are in our dataset?
-    num_points = 60
-    
+    num_points = 10000
+
     # For each of those points how many dimensions do they have?
     dimensions = 784
-    
+
     # Bounds for the values of those points in each dimension
     lower = 0
     upper = 256
-    
+
     # The K in k-means. How many clusters do we assume exist?
     num_clusters = 10
-    
+
     # When do we say the optimization has 'converged' and stop updating clusters
     opt_cutoff = 10.0
-    
+
     # Generate some points
 
     #points = [makeRandomPoint(dimensions, lower, upper) for i in xrange(num_points)]
@@ -63,26 +64,50 @@ def main():
     mn.test(0)
     index = 0
     for x in mn.train_images:
-        if index == 59:
+        if index == num_points-1:
             break
-        points.append(Point(x))
-        print index
+        p = Point(x)
+        p.add_label(mn.train_labels[index])
+        points.append(p)
+        #print index
         index = index + 1
     # Cluster those data!
     clusters = kmeans(points, num_clusters, opt_cutoff)
- 
+
     # Print our clusters
+    error_count = 0
+    # A list keep track which cluster represent which digits
+    clusters_num_lst = []
     for i,c in enumerate(clusters):
+        lst = []
+        for p in c.points:
+            lst.append(p.label)
+        tmp = Counter(lst)
+        # get the most common digit that classified to this cluster
+        num = tmp.most_common(1)[0][0]
+        if num in clusters_num_lst:
+            num = tmp.most_common(2)[1][0]
+        clusters_num_lst.append(num)
+        index = 0
         for p in c.points:
             print " Cluster: ", i, "\t"# Point :", p
             print mn.display(p.coords)
-    
+            print "Actual Label is : ", p.label
+            print "Predicted Label is : ", clusters_num_lst[index]
+            if num != int(p.label):
+                error_count = error_count + 1
+        index = index + 1
+
+    print clusters_num_lst
+    print "error count is ", error_count
+    print "error rate is ", error_count/float(num_points)
+
     # Display clusters using plotly for 2d data
     # This uses the 'open' command on a URL and may only work on OSX.
     if dimensions == 2 and PLOTLY_USERNAME:
         print "Plotting points, launching browser ..."
         plotClusters(clusters)
- 
+
 class Point:
     '''
     An point in n dimensional space
@@ -91,43 +116,46 @@ class Point:
         '''
         coords - A list of values, one per dimension
         '''
-        
+
         self.coords = coords
         self.n = len(coords)
-        
+        self.label = ""
+    def add_label(self, label):
+        self.label = label
+
     def __repr__(self):
         return str(self.coords)
- 
+
 class Cluster:
     '''
     A set of points and their centroid
     '''
-    
+
     def __init__(self, points):
         '''
         points - A list of point objects
         '''
-        
+
         if len(points) == 0: raise Exception("ILLEGAL: empty cluster")
         # The points that belong to this cluster
         self.points = points
-        
+
         # The dimensionality of the points in this cluster
         self.n = points[0].n
-        
+
         # Assert that all points are of the same dimensionality
         for p in points:
             if p.n != self.n: raise Exception("ILLEGAL: wrong dimensions")
-            
+
         # Set up the initial centroid (this is usually based off one point)
         self.centroid = self.calculateCentroid()
-        
+
     def __repr__(self):
         '''
         String representation of this object
         '''
         return str(self.points)
-    
+
     def update(self, points):
         '''
         Returns the distance between the previous centroid and the new after
@@ -136,9 +164,9 @@ class Cluster:
         old_centroid = self.centroid
         self.points = points
         self.centroid = self.calculateCentroid()
-        shift = getDistance(old_centroid, self.centroid) 
+        shift = getDistance(old_centroid, self.centroid)
         return shift
-    
+
     def calculateCentroid(self):
         '''
         Finds a virtual center point for a group of n-dimensional points
@@ -150,17 +178,17 @@ class Cluster:
         unzipped = zip(*coords)
         # Calculate the mean for each dimension
         centroid_coords = [math.fsum(dList)/numPoints for dList in unzipped]
-        
+
         return Point(centroid_coords)
- 
+
 def kmeans(points, k, cutoff):
-    
+
     # Pick out k random points to use as our initial centroids
     initial = random.sample(points, k)
-    
+
     # Create k clusters using those centroids
     clusters = [Cluster([p]) for p in initial]
-    
+
     # Loop through the dataset until the clusters stabilize
     loopCounter = 0
     while True:
@@ -168,7 +196,7 @@ def kmeans(points, k, cutoff):
         # Create a list of lists to hold the points in each cluster
         lists = [ [] for c in clusters]
         clusterCount = len(clusters)
-        
+
         # Start counting loops
         loopCounter += 1
         # For every point in the dataset ...
@@ -176,10 +204,10 @@ def kmeans(points, k, cutoff):
             # Get the distance between that point and the centroid of the first
             # cluster.
             smallest_distance = getDistance(p, clusters[0].centroid)
-        
+
             # Set the cluster this point belongs to
             clusterIndex = 0
-        
+
             # For the remainder of the clusters ...
             for i in range(clusterCount - 1):
                 # calculate the distance of that point to each other cluster's
@@ -192,23 +220,23 @@ def kmeans(points, k, cutoff):
                     smallest_distance = distance
                     clusterIndex = i+1
             lists[clusterIndex].append(p)
-        
+
         # Set our biggest_shift to zero for this iteration
         biggest_shift = 0.0
-        
+
         # As many times as there are clusters ...
         for i in range(clusterCount):
             # Calculate how far the centroid moved in this iteration
             shift = clusters[i].update(lists[i])
             # Keep track of the largest move from all cluster centroid updates
             biggest_shift = max(biggest_shift, shift)
-        
+
         # If the centroids have stopped moving much, say we're done!
         if biggest_shift < cutoff:
             print "Converged after %s iterations" % loopCounter
             break
     return clusters
- 
+
 def getDistance(a, b):
     '''
     Euclidean distance between two n-dimensional points.
@@ -216,10 +244,10 @@ def getDistance(a, b):
     '''
     if a.n != b.n:
         raise Exception("ILLEGAL: non comparable points")
-    
+
     ret = reduce(lambda x,y: x + pow((a.coords[y]-b.coords[y]), 2),range(a.n),0.0)
     return math.sqrt(ret)
- 
+
 def makeRandomPoint(n, lower, upper):
     '''
     Returns a Point object with n dimensions and values between lower and
@@ -227,18 +255,18 @@ def makeRandomPoint(n, lower, upper):
     '''
     p = Point([random.uniform(lower, upper) for i in range(n)])
     return p
- 
+
 def plotClusters(data):
     '''
     Use the plotly API to plot data from clusters.
-    
+
     Gets a plot URL from plotly and then uses subprocess to 'open' that URL
     from the command line. This should open your default web browser.
     '''
-    
-    # List of symbols each cluster will be displayed using    
+
+    # List of symbols each cluster will be displayed using
     symbols = ['circle', 'cross', 'triangle-up', 'square']
- 
+
     # Convert data into plotly format.
     traceList = []
     for i, c in enumerate(data):
@@ -261,10 +289,10 @@ def plotClusters(data):
         centroid['marker']['color'] = 'rgb(200,10,10)'
         centroid['name'] = "Centroid " + str(i)
         traceList.append(centroid)
-    
+
     # Log in to plotly
     py = plotly(username=PLOTLY_USERNAME, key=PLOTLY_KEY)
- 
+
     # Style the chart
     datastyle = {'mode':'markers',
              'type':'scatter',
@@ -272,12 +300,12 @@ def plotClusters(data):
                        'size':12,
                        'opacity':0.6,
                        'color':'rgb(74, 134, 232)'}}
-    
+
     resp = py.plot(*traceList, style = datastyle)
-    
+
     # Display that plot in a browser
     cmd = "open " + resp['url']
     subprocess.call(cmd, shell=True)
- 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     main()
--- sample.py
+++ sample.py
@@ -4,6 +4,7 @@
 
 import numpy
 import matplotlib
+import matplotlib.pyplot as plt
 import random
 import numpy as np
 matplotlib.use('Agg')
@@ -13,6 +14,33 @@ import pylab
 pylab.close()
 
 true_center = []
+#path_to_image = '/Users/margaretmw3/Desktop/kmeans.png'
+path_to_image = '/Users/ruiqingqiu/Desktop/kmeans.png'
+
+# Taking in k clusters, true_center list, and the result center list
+def error_calculate(k,true_center,res):
+    error = 0.0
+    for i in range(k):
+        min_dis = 1000000
+        for j in range(3):
+            dst = distance.euclidean(true_center[i],res[j])
+            if dst < min_dis:
+                min_dis = dst
+        error += min_dis
+    print "the error rate is: ", error
+    return error
+
+def plot_graph():
+    colors = ([([1,0,0],[0,1,0],[0,0,1])[i] for i in idx])
+    # plot colored points
+    pylab.scatter(dataSet[:,0],dataSet[:,1], c=colors)
+
+    # mark centroids as (X)
+    pylab.scatter(res[:,0],res[:,1], marker='o', s = 500, linewidths=2, c='none')
+    pylab.scatter(res[:,0],res[:,1], marker='x', s = 500, linewidths=2)
+    pylab.savefig(path_to_image)
+
+
 
 def init_board(N):
     X = np.array([(random.uniform(-1, 1), random.uniform(-1, 1)) for i in range(N)])
@@ -33,57 +61,23 @@ def init_board_gauss(N, k):
                 x.append([a,b])
         X.extend(x)
     X = np.array(X)[:N]
-    print true_center
+    #print true_center
     return X
 
-path_to_image = '/Users/margaretmw3/Desktop/kmeans.png'
-# generate 3 sets of normally distributed points around
-# different means with different variances
-# pt1 = numpy.random.normal(1, 0.2, (100,2))
-# pt2 = numpy.random.normal(2, 0.5, (300,2))
-# pt3 = numpy.random.normal(3, 0.2, (200,2))
-# pt1 = numpy.random.normal(1, 0.2, (20,2))
-# pt2 = numpy.random.normal(2, 0.5, (10,2))
-# pt3 = numpy.random.normal(3, 0.2, (10,2))
-
-# slightly move sets 2 and 3 (for a prettier output)
-#pt2[:,0] += 1
-#pt3[:,0] -= 0.5
-
-
-#res, idx = kmeans2(numpy.array(zip(xy[:,0],xy[:,1])),3)
-#xy = numpy.concatenate((pt1, pt2, pt3))
-
+num_of_iteration = 1000
 #res, idx = kmeans2(numpy.array(zip(xy[:,0],xy[:,1])),5)
-#dataSet = init_board(300) # create the initial configuration of the board
 dataSet = init_board_gauss(300,3)
-#print dataSet
-# kmeans for 5 clusters
-res, idx = kmeans2(dataSet,3)
-print res
-
-error = 0.0
-# measure how good is the k-mean
-for i in range(3):
-    min_dis = 1000000
-    for j in range(3):
-        dst = distance.euclidean(true_center[i],res[j])
-        if dst < min_dis:
-            min_dis = dst
-    error += min_dis
-print error
+error_list = []
+index = []
+for i in range(num_of_iteration):
+    res, idx = kmeans2(dataSet,3)
+    error_list.append(error_calculate(3,true_center,res))
+    index.append(i)
 
 
+plt.plot(index,error_list,'ro')
+#plt.ylabel("error rate")
+plt.show()
+plot_graph()
 
-
-colors = ([([1,0,0],[0,1,0],[0,0,1])[i] for i in idx])
-
-# plot colored points
-pylab.scatter(dataSet[:,0],dataSet[:,1], c=colors)
-
-# mark centroids as (X)
-pylab.scatter(res[:,0],res[:,1], marker='o', s = 500, linewidths=2, c='none')
-pylab.scatter(res[:,0],res[:,1], marker='x', s = 500, linewidths=2)
-#print "here"
-#pylab.savefig(path_to_image)
 
