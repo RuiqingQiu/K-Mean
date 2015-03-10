9d7a8fa81397915f0b8cf9dd9fe6d9e788b6ee9e
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-03-09 23:19:26 -0700

    mnist with cost

--- test.py
+++ test.py
@@ -1,47 +1,29 @@
-import os, struct
-from array import array as pyarray
-import numpy as np
-from numpy import append, array, int8, uint8, zeros
+from sklearn.datasets import fetch_mldata
+from sklearn.cluster import KMeans
+from sklearn.utils import shuffle
+import matplotlib
+import pylab
+import matplotlib.pyplot as plt
+mnist = fetch_mldata('MNIST original', data_home='./data')
+X_digits, _,_, Y_digits = mnist.values() # fetch dataset from internet
+X_digits, Y_digits = shuffle(X_digits,Y_digits) # shuffle dataset (which is ordered!)
+X_digits = X_digits[-5000:]
+plt.rc("image", cmap="binary") # use black/white palette for plotting
+for i in xrange(10):
+    plt.subplot(2,5,i+1)
+    plt.imshow(X_digits[i].reshape(28,28))
+    plt.xticks(())
+    plt.yticks(())
+plt.show()
 
-def load_mnist(dataset="training", digits=np.arange(10), path="."):
-    """
-    Loads MNIST files into 3D numpy arrays
+kmeans = KMeans(20)
+mu_digits = kmeans.fit(X_digits).cluster_centers_
 
-    Adapted from: http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
-    """
 
-    if dataset == "training":
-        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
-    elif dataset == "testing":
-        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
-    else:
-        raise ValueError("dataset must be 'testing' or 'training'")
-
-    flbl = open(fname_lbl, 'rb')
-    magic_nr, size = struct.unpack(">II", flbl.read(8))
-    lbl = pyarray("b", flbl.read())
-    flbl.close()
-
-    fimg = open(fname_img, 'rb')
-    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
-    img = pyarray("B", fimg.read())
-    fimg.close()
-
-    ind = [ k for k in range(size) if lbl[k] in digits ]
-    N = len(ind)
-
-    images = zeros((N, rows, cols), dtype=uint8)
-    labels = zeros((N, 1), dtype=int8)
-    for i in range(len(ind)):
-        images[i] = array(img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
-        labels[i] = lbl[ind[i]]
-
-    return images, labels
-from pylab import *
-from numpy import *
-print "hello"
-images, labels = load_mnist('training', digits=[2])
-imshow(images.mean(axis=0), cmap=cm.gray)
-show()
+plt.figure(figsize=(16,6))
+for i in xrange(2*(mu_digits.shape[0]/2)): # loop over all means
+    plt.subplot(2,mu_digits.shape[0]/2,i+1)
+    plt.imshow(mu_digits[i].reshape(28,28))
+    plt.xticks(())
+    plt.yticks(())
+plt.show()
